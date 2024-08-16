ec7428a589f1f3b51804ff29fb5ce45b54499344
alunyapatrick2022 <patrickalunya106@gmail.com>
2024-08-16 11:13:32 +0300

    v1

---
+++ user-inputs.py
@@ -0,0 +1,28 @@
+# Functions to collect user case data
+
+def input(
+    prompt: object = "",
+    /
+) -> str:
+    first_name = ""
+
+def input(
+    prompt: object = "",
+    /
+) -> str:
+    last_name = ""
+
+def input(
+    prompt: object = "",
+    /
+) -> str:
+    email = ""
+
+def input(
+    prompt: object = "",
+    /
+) -> str:
+    location = ""
+
+    print(input("Hello, {first_name} {last_name}, your email is {email} and you live in {location} "))
+
