13739a691e36b93099dfef802999e614c52ec043
alunyapatrick2022 <patrickalunya106@gmail.com>
2024-08-16 11:22:12 +0300

    v2

--- user-inputs.py
+++ user-inputs.py
@@ -1,6 +1,7 @@
 def collect_user_data():
     first_name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
+    age = input("Enter your age: ")
     email = input("Enter your email: ")
     location = input("Enter your location: ")
 
